608b3bc3372500fca21cb7e9c4d8393a90b3b7fd
unknown <aushedo@spartaglobal.com>
2019-01-22 11:14:04 +0000

    dont merge this

--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,7 @@
 require "pg"
 
 class User
+attr_accessor :user_id, :first_name, :last_name, :email, :password, :cohort_id, :role_id
 
   def self.open_connection
     return PG.connect(dbname: "user_management")
