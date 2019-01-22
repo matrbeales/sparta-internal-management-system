263b731d086b332a429847e3c05560b7a39f854f
unknown <aushedo@spartaglobal.com>
2019-01-22 17:11:30 +0000

    saving roles

--- controllers/roles_controller.rb
+++ controllers/roles_controller.rb
@@ -20,8 +20,8 @@ class RolesController < Sinatra::Base
   # NEW
   get "/new" do
     # @role
-
-
+    get "/new" do
+      @role = Role.new
     erb :"roles/new.html"
   end
 
@@ -30,37 +30,42 @@ class RolesController < Sinatra::Base
     # @role
     id = params[:id].to_i
     @role = Role.find id
-
     erb :"roles/show.html"
   end
 
   # EDIT
   get "/:id/edit" do
     # @role
-
-
+    id = params[:id].to_i
+    @role = Role.find id
     erb :"roles/edit.html"
   end
 
   # CREATE
   post "/" do
-
-
-    redirect "/"
+    post "/" do
+      role = Role.new
+      role.role_id = params[:role_id]
+      role.role_name = params[:role_name]
+      role.save
+    redirect "/roles"
   end
 
   # UPDATE
   put "/:id" do
-
-
-    redirect "/#{id}"
+    id = params[:id].to_i
+    role = Role.find id
+    role.role_id = params[:role_id]
+    role.role_name = params[:role_name]
+    role.save
+    redirect "/roles/#{id}"
   end
 
   # DESTROY
   delete "/:id" do
-
-
-    redirect "/"
+    id = params[:id].to_i
+    Role.destroy id
+    redirect "/roles"
   end
 
 
