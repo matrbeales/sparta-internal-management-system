b58d0cf1605455d5460875f1652d12ab915b4f6f
Laura Green <LGreen@spartaglobal.com>
2019-01-22 10:27:52 +0000

    new controller done

--- models/user.rb
+++ models/user.rb
@@ -1,9 +1,11 @@
 require "pg"
 
 class User
-
+attr_accessor :user_id, :first_name, :last_name, :email, :password, :cohort_id, :role_id
   def self.open_connection
     return PG.connect(dbname: "user_management")
   end
+
+  
 
 end
